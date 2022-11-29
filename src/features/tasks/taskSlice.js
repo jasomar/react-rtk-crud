b8d56d80f9862071eedb4d93bb60191dd7cfc0ad
Jafet Solano <youremail@domain.com>
2022-11-28 21:47:50 -0600

    fix bugs on deploy site

--- src/features/tasks/taskSlice.js
+++ src/features/tasks/taskSlice.js
@@ -1,46 +1,43 @@
 import {createSlice} from "@reduxjs/toolkit";
 
 const initialState = [
-    {
-        id:"1",
-        title: "Task 1",
-        description: "Task 1 description test 1",
-        completed: false
+  {
+    id: "1",
+    title: "Task 1",
+    completed: false,
+    description: "This is a task",
+  },
+  {
+    id: "2",
+    title: "Task 2",
+    completed: false,
+    description: "This is a task",
+  },
+];
+
+const userSlice = createSlice({
+  name: "tasks",
+  initialState,
+  reducers: {
+    addTask: (state, action) => {
+      state.push(action.payload);
     },
-    {
-        id:"2",
-        title: "Task 2",
-        description: "Task 2 description test 2",
-        completed: false
+    editTask: (state, action) => {
+      const { id, title, description } = action.payload;
+      const foundTask = state.find((task) => task.id === id);
+      if (foundTask) {
+        foundTask.title = title;
+        foundTask.description = description;
+      }
     },
-]
+    deleteTask: (state, action) => {
+      const foundTask = state.find((task) => task.id === action.payload);
+      if (foundTask) {
+        state.splice(state.indexOf(foundTask), 1);
+      }
+    },
+  },
+});
 
-export const taskSlice =  createSlice ({
-    name:'tasks',
-    initialState:initialState,
-    reducers:{ 
-        addTask: (state,action) => {
-          //[...state,action.payload]
-          state.push(action.payload)
-        },
-        deleteTask:(state,action) => {
-            const taskfound = state.find(task => task.id === action.payload)
-            if (taskfound){
-                state.splice(state.indexOf(taskfound),1)
-            }
-        },
-        editTask:(state,action) =>{
-            const {id,title,description} = action.payload
-            const foundTask = state.find(task => task.id === id)
-            console.log(action.payload)
-            if(foundTask){
-                foundTask.title = title
-                foundTask.description = description
-            }
-        }
-    }
-})
-
-
-export const {addTask,deleteTask,editTask} = taskSlice.actions
-export default taskSlice.reducer
+export const { addTask, editTask, deleteTask } = userSlice.actions;
+export default userSlice.reducer;
